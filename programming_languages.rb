d17ef357c4f989dc5d496c262f2e3cdf2b001f2b
Billie Simmons <billiesnyc@gmail.com>
2018-08-21 17:57:49 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -2,9 +2,9 @@ def reformat_languages(languages)
   new_hash = {}
   languages.each do |key, value|
     second_level.each do |key, value|
-    new_hash << [key]value    
+    new_hash[key] << value    
     end
-  new_hash << [value][:style]key
+  new_hash[value][:style] << key
   end
 end
 
